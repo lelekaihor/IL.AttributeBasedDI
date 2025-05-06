94de495ddd336a7e2f0f9b1dba42b1950bcede99
Ihor Leleka <ihorleleka@gmail.com>
2025-05-07 00:44:24 +0100

    feat: added features listing to tooltip of visualizer

---
+++ Code/IL.AttributeBasedDI.Visualizer/Converters/CustomTypeConverter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace IL.AttributeBasedDI.Visualizer.Converters;
+
+public sealed class CustomTypeConverter : JsonConverter
+{
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        // Only handle Type objects
+        if (value is Type type)
+        {
+            writer.WriteValue(type.FullName);
+        }
+        else
+        {
+            writer.WriteNull();
+        }
+
+    }
+
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+        return typeof(Type).IsAssignableFrom(objectType);
+    }
+}
